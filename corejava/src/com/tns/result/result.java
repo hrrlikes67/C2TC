1665117f8e7f3372bda59bf775853100264bbeb5
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-09-14 17:57:08 +0530

    oops

---
+++ corejava/src/com/tns/result/result.java
@@ -0,0 +1,14 @@
+package com.tns.result;
+import com.tns.calculate.*;
+public class result {
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Sum s = new Sum();
+	      s.cal();
+
+	}
+
+}
