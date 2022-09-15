d71232c0cd909809e436267c80ee7c1cf83a3191
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-09-15 19:37:39 +0530

    assignment

---
+++ com.tns.assignment/src/com/tns/assignment/assignment_light.java
@@ -0,0 +1,18 @@
+package com.tns.assignment;
+import java.util.Scanner;
+public class assignment_light {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+        int hrs=sc.nextInt();
+        if(hrs<18)
+        {
+            System.out.println("lights on");
+        }
+        else if(hrs<=24&&hrs>18){
+        System.out.println("lights off");    
+        }
+	}
+
+}
