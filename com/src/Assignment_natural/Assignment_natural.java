1ccd2bb41331d15ad4f421af10d496eab4b6d84d
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-09-15 21:47:23 +0530

    assignment natural

---
+++ com/src/Assignment_natural/Assignment_natural.java
@@ -0,0 +1,13 @@
+package Assignment_natural;
+import java.util.Scanner;
+public class Assignment_natural {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number;
+		for(number=1;number<=10;number++) {
+			System.out.println(number);
+		}
+	}
+
+}
