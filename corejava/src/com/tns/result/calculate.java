1b3a4c261cca95637c1b7e688bd3220defcf246b
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-09-15 18:57:27 +0530

    assignment

---
+++ corejava/src/com/tns/result/calculate.java
@@ -0,0 +1,5 @@
+package com.tns.result;
+
+public class calculate {
+
+}
--- corejava/src/com/tns/result/caluclate.java
+++ corejava/src/com/tns/result/caluclate.java
@@ -1,14 +1,11 @@
 package com.tns.result;
-import com.tns.calculate.*;
-public class result {
 
-
+public class caluclate {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 caluclate s = new caluclate();
-	      s.cal();
-
+		caluclate s=new caluclate();
+		   s.cal();
 	}
 
 }
