7178635490d41b93e8a474328237524294ff784e
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-09-19 11:23:01 +0530

    assignment 2

---
+++ com/src/Assignments2/main.java
@@ -0,0 +1,16 @@
+package Assignments2;
+
+public class main {
+	int x;
+
+	  // Constructor with a parameter
+	  public main(int x) {
+	    this.x = x;
+	  }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 main Ob = new main(6);
+		    System.out.println("Value of x = " + Ob.x);
+	}
+
+}
