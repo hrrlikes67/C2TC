de3ab9facc6527c25b11c360361f08259e7d6c75
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-09-22 08:35:36 +0530

    final

--- com/src/assignment/finalexamp/FinalVar.java
+++ com/src/assignment/finalexamp/FinalVar.java
@@ -6,14 +6,19 @@ void change()
 {
 	
 	a=40;
-	System.out.printIn(a);
+	System.out.println(a);
 	final int i=0;
 	for(i=0;i<5;i++)
-	{System.out.printIn("Value of I")
+	{
+		System.out.println("Value of I:"+i);
+	}
 }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		FinalVar fv=new FinalVar();
+       
+		fv.change();
 	}
 
 }
