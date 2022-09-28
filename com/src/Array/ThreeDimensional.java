926575bfcf4425cb291b50ff9194949393bd41f7
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-09-28 11:14:42 +0530

    arraysthree

---
+++ com/src/Array/ThreeDimensional.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class ThreeDimensional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+final int[][] matrix= {
+		{7, 4 ,1},
+		{1, 6, 5},
+		{8, 4, 4}
+		};
+for(int i=0;i< matrix.length;i++)
+{
+	for(int j=0;j<matrix.length;j++)
+	{
+		System.out.print(matrix[i][j]+" ");
+	}
+	System.out.println();
+}
+	}
+
+}
