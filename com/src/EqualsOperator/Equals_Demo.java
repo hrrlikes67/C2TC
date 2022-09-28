73648d0c060c09bc74b3202e03686bb6bb54d54d
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-09-28 11:48:33 +0530

    Equals

---
+++ com/src/EqualsOperator/Equals_Demo.java
@@ -0,0 +1,22 @@
+package EqualsOperator;
+
+public class Equals_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=7;
+int j=-1;
+int k=7;
+if(i==j) {
+	System.out.println("i and j are equal");
+}else {
+	System.out.println("i and j are not equal");
+}
+if(k==i) {
+	System.out.println("i and k are equal");
+}else {
+	System.out.println("i and k are not equal");
+}
+	}
+
+}
