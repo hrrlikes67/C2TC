85387b58fa3391de3fcc26e6690b9ad192846530
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-09-28 10:15:48 +0530

    MultidimesionArray

---
+++ com/src/Array/MultiDimensions.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class MultiDimensions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[][] mynumbers= {{2,4},{1,2}};
+for(int i=0;i<mynumbers.length;++i)
+{
+	for(int j=0;j<mynumbers[i].length;++j);
+	{
+		System.out.println(mynumbers[i][j]);
+	}
+}
+	}
+
+}
