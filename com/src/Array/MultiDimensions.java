a0f0536d34fa344858f74e6840f7a71557f9b56e
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-09-28 11:20:04 +0530

    2array

--- com/src/Array/MultiDimensions.java
+++ com/src/Array/MultiDimensions.java
@@ -4,13 +4,16 @@ public class MultiDimensions {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int[][] mynumbers= {{2,4},{1,2}};
-for(int i=0;i<mynumbers.length;++i)
+int[][] mynumbers= {{2,4},
+		            {1,2}
+};
+for(int i=0;i<2;++i)
 {
-	for(int j=0;j<mynumbers[i].length;++j);
+	for(int j=0;j<2;++j)
 	{
-		System.out.println(mynumbers[i][j]);
+		System.out.print(mynumbers[i][j]+"  ");
 	}
+	System.out.println();
 }
 	}
 
