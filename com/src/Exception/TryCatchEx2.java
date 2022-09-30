ad38554fadb8933e11c116fe9b6138ed288ed3d7
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-09-30 09:10:46 +0530

    exception2

---
+++ com/src/Exception/TryCatchEx2.java
@@ -0,0 +1,21 @@
+package Exception;
+
+public class TryCatchEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("111");
+try
+{
+	int x=12/0;
+	System.out.println("Result of x:"+x);
+	System.out.println("333");
+}
+catch(ArithmeticException ae)
+{
+	System.out.println("Hell world");
+}
+System.out.println("444");
+	}
+
+}
