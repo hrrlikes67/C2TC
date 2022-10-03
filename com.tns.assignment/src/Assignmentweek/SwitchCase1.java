c3702e97c21a14bb855dc7dfcc41bdde203352b8
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-10-03 08:56:41 +0530

    SW

---
+++ com.tns.assignment/src/Assignmentweek/SwitchCase1.java
@@ -0,0 +1,26 @@
+package Assignmentweek;
+
+public class SwitchCase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x = 10;
+		
+		switch (x)
+		{
+		case 1:
+		System.out.println("The value of x is 1");
+		break;
+		case 2:
+		System.out.println("The value of x is 2");
+		break;
+		case 3:
+		System.out.println("The value of x is 3");
+		break;
+		default:
+		System.out.println("The value of x is other than 1,2,3");
+		break;
+	}
+	}
+
+}
