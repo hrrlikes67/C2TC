bfb8e82672feb317ced5e20d718110f4e8f812a3
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-10-03 08:40:52 +0530

    DM

---
+++ com.tns.assignment/src/Assignmentweek/DecsionMaking1.java
@@ -0,0 +1,19 @@
+package Assignmentweek;
+
+public class DecsionMaking1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ageOfBoy = 36;
+		int ageOfGirl = 25;
+		if ((ageOfBoy >= 21) && (ageOfGirl >= 18))
+		{
+		System.out.println("Ready to get married;>");
+		}
+		else
+		{
+		System.out.println("Wait for it kiddo!");
+	}
+	}
+
+}
