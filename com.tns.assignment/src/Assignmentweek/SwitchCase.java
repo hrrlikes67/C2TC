73de7699bde741c288e1121c05ad21aaf9c3279a
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-10-03 08:55:12 +0530

    SW

--- com.tns.assignment/src/Assignmentweek/SwitchCase.java
+++ com.tns.assignment/src/Assignmentweek/SwitchCase.java
@@ -4,7 +4,23 @@ public class SwitchCase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String name = "TEAM";
 
+		switch (name.toLowerCase())
+		{
+		case "author":
+		System.out.println("Vikas");
+		break;
+		case "team":
+		System.out.println("Team Java Full Stack");
+		break;
+		case "editor":
+		System.out.println("Vishnu & Krishna");
+		break;
+		default:
+		System.out.println("Invalid entry");
+		break;
+	}
 	}
 
 }
