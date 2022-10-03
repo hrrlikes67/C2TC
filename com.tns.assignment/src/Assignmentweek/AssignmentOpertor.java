bcd2a5a51b5e78d4386072239646dce801f1585d
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-10-03 08:31:22 +0530

    Assi.operator

---
+++ com.tns.assignment/src/Assignmentweek/AssignmentOpertor.java
@@ -0,0 +1,13 @@
+package Assignmentweek;
+
+public class AssignmentOpertor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 4;
+		x %= 5; // x = x % 5
+		
+		System.out.println(x); // % is the remainder on division
+	}
+
+}
