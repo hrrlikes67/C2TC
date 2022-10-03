4b2fae4987e2b0762d4b39f68f61ff480e423452
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-10-03 08:59:33 +0530

    Ternarycode

---
+++ com.tns.assignment/src/Assignmentweek/Ternary.java
@@ -0,0 +1,12 @@
+package Assignmentweek;
+
+public class Ternary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		x = (10 == x) ? 1 : 0;
+		System.out.println(x);
+	}
+
+}
