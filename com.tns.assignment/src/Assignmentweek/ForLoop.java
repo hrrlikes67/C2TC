8aed5af8b7c08055f35efff1205ae29bde0234f0
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-10-03 08:52:13 +0530

    Nestedloop

--- com.tns.assignment/src/Assignmentweek/ForLoop.java
+++ com.tns.assignment/src/Assignmentweek/ForLoop.java
@@ -4,7 +4,26 @@ public class ForLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int number = 9;
+		boolean isPrime = true;
+		for(int i = 2; i < number/2; i++)
+		{
+		if(number % i == 0)
+		{
+		isPrime = false;	
+		}
+		}
+		if(isPrime == true)
+		{
 
+		System.out.println(number+" is a prime number");
+		}
+		else
+		{
+
+		System.out.println(number+" is not a prime number");
+
+		}
 	}
 
 }
