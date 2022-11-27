cf25dd8a0b7621601a40f793e28451b226e44672
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-11-27 16:46:19 +0530

    Hibernet jdbc

---
+++ Hb_jpa_operations/src/com/tns/dao/studentdaoimplementation.java
@@ -0,0 +1,31 @@
+package com.tns.dao;
+
+import com.tns.entity.student;
+
+public class studentdaoimplementation {
+
+	public  void addstudent(student student)
+	{
+		
+	}
+    public void getstudentbyId(int id)
+    {
+    	return;
+    }
+    public   void updatestudent(student student)
+    {
+    	
+    }
+    public   void removestudent(student student)
+    {
+    	
+    }
+    public   void commitTransaction()
+    {
+    	
+    }
+    public   void beginTransaction()
+    {
+    	
+    }
+}
