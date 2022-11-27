fd9b87227926e950d9fca6e4591daa4262f81ab2
ADMIN <ADMIN@DESKTOP-QHT2RN6>
2022-11-27 16:57:25 +0530

    studnt

--- Hb_jpa_operations/src/com/tns/dao/studentdaoimplementation.java
+++ Hb_jpa_operations/src/com/tns/dao/studentdaoimplementation.java
@@ -4,28 +4,30 @@ import com.tns.entity.student;
 
 public class studentdaoimplementation {
 
+	private EntityManager entityManager;
+	
 	public  void addstudent(student student)
 	{
-		
+		entityManager.persist(student);
 	}
     public void getstudentbyId(int id)
     {
-    	return;
+    	return entityManager.find(student.class,id);
     }
     public   void updatestudent(student student)
     {
-    	
+    	entityManager.merge(student);
     }
     public   void removestudent(student student)
     {
-    	
+    	entityManager.remove(student);
     }
     public   void commitTransaction()
     {
-    	
+    	entityManager.getTransaction().commit();
     }
     public   void beginTransaction()
     {
-    	
+    	entityManager.getTransaction().begin();	
     }
 }
